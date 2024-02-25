734a9eb614324dcfcaee7edfaa20c1315dd46481
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2024-02-25 02:16:47 +0200

    localstor

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,7 +17,7 @@ const persistConfig = {
   key: 'root',
   version: 1,
   storage,
-  whitelist: ['modal', 'page', 'filteredCars'],
+  blacklist: ['modal', 'page', 'filteredCars'],
 };
 
 const persistedReducer = persistReducer(persistConfig, carsReducer);
