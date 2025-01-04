a77829a454876a3ef75686b1c569224a1318ab94
julika-gulchitai <127190696+julika-gulchitai@users.noreply.github.com>
2025-01-04 23:41:09 +0200

    Update RunLine.jsx

--- src/components/RunLine/RunLine.jsx
+++ src/components/RunLine/RunLine.jsx
@@ -7,7 +7,7 @@ import mg from '../../Images/brands/mg.webp';
 import mitsubishi from '../../Images/brands/mitsubishi.webp';
 import buick from '../../Images/brands/buick.webp';
 import lincoln from '../../Images/brands/lincoln.webp';
-import hummer from '../../Images/brands/hummer.webp';
+//import hummer from '../../Images/brands/hummer.webp';
 import volvo from '../../Images/brands/volvo.webp';
 import nissan from '../../Images/brands/nissan.webp';
 import { LineWrap, StyledUl } from './RunLineStyled';
